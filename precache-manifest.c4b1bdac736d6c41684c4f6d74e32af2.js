68a59a2d75d74195f82339b7002d3bc452912d93
jason-toups <me@jasontoups.com>
2020-07-07 11:35:36 -0700

    Updates

---
+++ precache-manifest.c4b1bdac736d6c41684c4f6d74e32af2.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da2b3b5a2e8538b7e2d318b82d7db57",
+    "url": "/werk/index.html"
+  },
+  {
+    "revision": "f84a8ed2c1df2a39963e",
+    "url": "/werk/static/css/2.330c2626.chunk.css"
+  },
+  {
+    "revision": "0e51443429f1fc139dff",
+    "url": "/werk/static/css/main.97a5ea18.chunk.css"
+  },
+  {
+    "revision": "f84a8ed2c1df2a39963e",
+    "url": "/werk/static/js/2.33362b38.chunk.js"
+  },
+  {
+    "revision": "adbe8770dfe80185b1d2467350068428",
+    "url": "/werk/static/js/2.33362b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e51443429f1fc139dff",
+    "url": "/werk/static/js/main.000c0e5c.chunk.js"
+  },
+  {
+    "revision": "a37e7acf30c82f47eea5",
+    "url": "/werk/static/js/runtime-main.2edf6739.js"
+  },
+  {
+    "revision": "79ae476b20eeb73d7241e42fd9bf1222",
+    "url": "/werk/static/media/Werk-Logo.79ae476b.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/werk/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/werk/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/werk/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/werk/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/werk/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "39235e9446b92187414ee25ae52d9fc4",
+    "url": "/werk/static/media/dollar-empty.39235e94.svg"
+  },
+  {
+    "revision": "c5abad7024c9222dcbbf6843ff0dcc5b",
+    "url": "/werk/static/media/dollar-filled.c5abad70.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/werk/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "1ed115c7828f5c8ec2568e93a1f23ff0",
+    "url": "/werk/static/media/heart-empty.1ed115c7.svg"
+  },
+  {
+    "revision": "5427151c93de4dd1e3596be6c4e4d682",
+    "url": "/werk/static/media/heart-filled.5427151c.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/werk/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/werk/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/werk/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/werk/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/werk/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "7f054c2a9774963e4fb365207e016154",
+    "url": "/werk/static/media/ineedahero-edit.7f054c2a.gif"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/werk/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/werk/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/werk/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/werk/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/werk/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "783543eb36a35b4694844311ef8cae7e",
+    "url": "/werk/static/media/repeatablePattern-red.783543eb.svg"
+  }
+]);
